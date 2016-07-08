a9ed6f3e3dde01b7c4e10f86dfe01d979472ac78
Tommy Jones <tommyinseason@yahoo.com>
2016-07-08 14:24:24 -0700

    Adding Yelp Keys

---
+++ app/src/main/java/com/example/guest/gobal/Constants.java
@@ -0,0 +1,11 @@
+package com.example.guest.gobal;
+
+/**
+ * Created by Guest on 7/8/16.
+ */
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}

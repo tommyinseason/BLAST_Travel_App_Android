45f3645c594330ff78e2c124145f4debeba06a12
Tommy Jones <tommyinseason@yahoo.com>
2016-07-16 20:40:09 -0700

    Added Login And Sign Up page

---
+++ app/src/main/java/com/example/guest/gobal/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.guest.gobal.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guest.gobal.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

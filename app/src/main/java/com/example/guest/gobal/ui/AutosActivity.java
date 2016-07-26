b885081d13fb98581efc0e83ba22d957d63b53bc
Tommy Jones <tommyinseason@yahoo.com>
2016-07-26 09:36:23 -0700

    Took our slide bar so it works now

--- app/src/main/java/com/example/guest/gobal/ui/AutosActivity.java
+++ app/src/main/java/com/example/guest/gobal/ui/AutosActivity.java
@@ -1,15 +1,27 @@
 package com.example.guest.gobal.ui;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.guest.gobal.R;
 
+import butterknife.ButterKnife;
+
 public class AutosActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +34,7 @@ public class AutosActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own ???", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
